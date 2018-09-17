ed3fcf2b168541fe48e69bbf71553ddaa5e60a2e
王宁 <wangning12@100tal.com>
2018-09-17 21:29:51 +0800

    组件name使用驼峰命名格式

--- src/libs/create-template.ts
+++ src/libs/create-template.ts
@@ -1,40 +1,44 @@
-import * as vscode from 'vscode'
-import fs = require('fs')
-import path = require('path')
-import toast from './toast'
-
+import * as vscode from "vscode";
+import fs = require("fs");
+import path = require("path");
+import toast from "./toast";
+const camelCase = require('camelcase');
 export default async function(fsPath: string) {
   try {
     let name = await vscode.window.showInputBox({
-      prompt: '请输入组件名称'
-    })
-    name = (name || '').trim()
+      prompt: "请输入组件名称"
+    });
+    name = (name || "").trim();
     if (!name) {
-      return toast.error('请输入组件名称')
+      return toast.error("请输入组件名称");
     }
-    const pick = await vscode.window.showQuickPick(['yes', 'no'], {
-      placeHolder: '是否需要新建文件夹'
+    const pick = await vscode.window.showQuickPick(["yes", "no"], {
+      placeHolder: "是否需要新建文件夹"
+    });
+    let filename = name;
+    if (pick === "yes") {
+      fs.mkdirSync(path.resolve(fsPath, name));
+      filename = `${name}/index`;
+    }
+    const filePath = path.resolve(fsPath, `${filename}.vue`);
+    
+    const componentName = camelCase(name, {
+      pascalCase: true
     })
-    let filename = name
-    if (pick === 'yes') {
-      fs.mkdirSync(path.resolve(fsPath, name))
-      filename = `${name}/index`
-    }
-    const filePath = path.resolve(fsPath, `${filename}.vue`)
     const str = `<template>
-    <div class="${name}-wrap"></div>
+  <div class="${name}-wrap"></div>
 </template>
 
 <script>
 export default {
-  name: '${name}'
+  name: '${componentName}'
 }
 </script>
 <style lang="scss" scoped></style>
-`
-    fs.writeFileSync(filePath, str, 'utf8')
+`;
+    fs.writeFileSync(filePath, str, "utf8");
   } catch (error) {
-    console.error(error)
-    toast.error(error.message)
+    console.error(error);
+    toast.error(error.message);
   }
 }
